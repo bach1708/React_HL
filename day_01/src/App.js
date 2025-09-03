f138c76cd9a68c99274ed4e3c32663da36cf28e3
bach1708 <kunkontkls@gmail.com>
2025-09-03 13:54:35 +0700

    First commit - add day_01 project

---
+++ day_01/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Nông Văn Bách </h1>
+      <hr/>
+      <div className="p-3 bg-primary text-white rounded">
+        Mã sinh viên: 2210900003
+   
+      </div>
+    </div>
+  );
+}
+
+export default App;
